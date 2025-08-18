11fab0e52c4951aecad422c0c892c9a071591a0d
Isac Ribeiro <isackolling122@gmail.com>
2025-08-18 19:08:18 -0300

    feat: configura DbContext, cria a camada Data e adiciona o migration inicial

---
+++ src/NovoMobile.Api/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NovoMobile.Api.Data
+{
+    // Herança de DbContext
+    public class AppDbContext : DbContext 
+    {
+        // Construtor que recebe as opções de configuração do DbContext como: Nome do banco, string de conexão, servidor, etc.
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base (options)
+        {           
+        }
+
+        // Propriedade que referencia a tabela de veículos a partir do Models Estacionamento
+        public DbSet<Models.Estacionamento> VeiculosEstacionados { get; set; }
+    }
+}
---
+++ src/NovoMobile.Api/Migrations/20250818215338_FirstMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NovoMobile.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class FirstMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "VeiculosEstacionados",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Placa = table.Column<string>(type: "nvarchar(7)", maxLength: 7, nullable: false),
+                    Modelo = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Observacao = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: true),
+                    DataEntrada = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DataSaida = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VeiculosEstacionados", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VeiculosEstacionados");
+        }
+    }
+}
